965ac86a030e22744b798665484bdfbe10c81a62
arash rad <arash1980rad@gmail.com>
2017-04-06 10:10:03 -0400

    Adding Logistic regression class to the code, removing L1 regularizer and adding L2 regularizer

--- mlp1_ed.py
+++ mlp1_ed.py
@@ -1,156 +1,49 @@
+# ***The code for MLP classifier is taken from theano's tutorial***
+
 from __future__ import print_function
 
 __docformat__ = 'restructedtext en'
 
-
 import os
 import sys
 import timeit
-from scipy.ndimage.interpolation import map_coordinates
-from scipy.ndimage.filters import gaussian_filter
-
 import numpy
-import numpy as np
 import gzip
 import theano
 import theano.tensor as T
 import six.moves.cPickle as pickle
 from theano.tensor.shared_randomstreams import RandomStreams
-
-
-from logistic_regression import LogisticRegression
-
-
-def elastic_transform(image, alpha, sigma, random_state=None):
-    """Elastic deformation of images as described in [Simard2003]_.
-    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
-       Convolutional Neural Networks applied to Visual Document Analysis", in
-       Proc. of the International Conference on Document Analysis and
-       Recognition, 2003.
-    """
-    assert len(image.shape) == 2
-
-    if random_state is None:
-        random_state = np.random.RandomState(None)
-
-    shape = image.shape
-
-    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-
-    x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
-    indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
-
-
-    return map_coordinates(image, indices, order=1).reshape(shape)
-
-
-def image_set_distortion(image_set):
-    temp = []
-    # counter = 0
-    for image in image_set:
-        image = image.reshape(28, 28)
-        image = elastic_transform(image, 36, 8)
-        image = image.flatten()
-        temp.append(image)
-        # print(counter)
-        # counter +=1
-    return np.array(temp).reshape(50000, 784)
+# from logistic_regression import LogisticRegression
 
 
 def load_data(dataset):
-    ''' Loads the dataset
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    '''
-
-    #############
-    # LOAD DATA #
-    #############
-
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "data",
-            dataset
-        )
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
-
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        from six.moves import urllib
-        origin = (
-            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        )
-        print('Downloading data from %s' % origin)
-        urllib.request.urlretrieve(origin, dataset)
-
-    print('... loading data')
+    """function to load Mnist dataset"""
 
     # Load the dataset
+    print('... loading data')
     with gzip.open(dataset, 'rb') as f:
-        try:
-            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
-        except:
-            train_set, valid_set, test_set = pickle.load(f)
-    # train_set, valid_set, test_set format: tuple(input, target)
-    # input is a numpy.ndarray of 2 dimensions (a matrix)
-    # where each row corresponds to an example. target is a
-    # numpy.ndarray of 1 dimension (vector) that has the same length as
-    # the number of rows in the input. It should give the target
-    # to the example with the same index in the input.
+        train_set, valid_set, test_set = pickle.load(f)
 
+    # creating shared variables from dataset
+    print('... creating shared variables ')
     def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
+        """ Function that loads the dataset into shared variables"""
 
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
         data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
+        shared_x = theano.shared(numpy.asarray(data_x, dtype=theano.config.floatX), borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y, dtype=theano.config.floatX), borrow=borrow)
         return shared_x, T.cast(shared_y, 'int32')
-
-    # train_set_x, train_set_y = train_set
-    # train_set_x_copy = train_set_x.copy()
-    # print(np.shape(train_set_x_copy))
-    # print(" ... data augmentation")
-    # train_set_x_copy = image_set_distortion(train_set_x_copy)
-    # train_set_y = np.concatenate((train_set_y, train_set_y), axis=0)
-    # train_set_x = np.concatenate((train_set_x, train_set_x_copy), axis=0)
-    # print(np.shape(train_set_x))
-    # train_set = (train_set_x, train_set_y)
 
     test_set_x, test_set_y = shared_dataset(test_set)
     valid_set_x, valid_set_y = shared_dataset(valid_set)
     train_set_x, train_set_y = shared_dataset(train_set)
 
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-            (test_set_x, test_set_y)]
-    return rval
+    return_values = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
+    return return_values
 
 
 def records_file_maker(name, lst1, lst2, test_val):
-    """ this write given list into a .csv file"""
+    """ this write given lists into a .csv file"""
     import csv
     with open(name, 'w') as csv_file:
         file_writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
@@ -160,18 +53,96 @@ def records_file_maker(name, lst1, lst2, test_val):
         file_writer.writerow(['best test score', test_val])
     return None
 
-# start-snippet-1
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
-                 activation=T.nnet.relu):
+
+class LogisticRegression(object):
+    """Multi-class Logistic Regression Class from theano's tutorial"""
+
+
+    def __init__(self, input, n_in, n_out):
+        """ Initialize the parameters of the logistic regression
+
+        :type input: theano.tensor.TensorType
+        :param input: symbolic variable that describes the input of the
+                      architecture (one minibatch)
+
+        :type n_in: int
+        :param n_in: number of input units, the dimension of the space in
+                     which the datapoints lie
+
+        :type n_out: int
+        :param n_out: number of output units, the dimension of the space in
+                      which the labels lie
+
         """
-        Typical hidden layer of a MLP: units are fully-connected and have
-        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
+        # start-snippet-1
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        self.W = theano.shared(value=numpy.zeros((n_in, n_out), dtype=theano.config.floatX), name='W', borrow=True)
+        # initialize the biases b as a vector of n_out 0s
+        self.b = theano.shared(value=numpy.zeros((n_out,), dtype=theano.config.floatX), name='b', borrow=True)
+
+        # symbolic expression for computing the matrix of class-membership
+        # probabilities
+        # Where:
+        # W is a matrix where column-k represent the separation hyperplane for
+        # class-k
+        # x is a matrix where row-j  represents input training sample-j
+        # b is a vector where element-k represent the free parameter of
+        # hyperplane-k
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+        # symbolic description of how to compute prediction as class whose
+        # probability is maximal
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        # end-snippet-1
+
+        # parameters of the model
+        self.params = [self.W, self.b]
+
+        # keep track of model input
+        self.input = input
+
+    def negative_log_likelihood(self, y):
+        """Return the mean of the negative log-likelihood of the prediction
+        of this model under a given target distribution.
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+
+        Note: we use the mean instead of the sum so that
+              the learning rate is less dependent on the batch size
+        """
+        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
+        # number of examples (call it n) in the minibatch
+        # T.arange(y.shape[0]) is a symbolic vector which will contain
+        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
+        # Log-Probabilities (call it LP) with one row per example and
+        # one column per class LP[T.arange(y.shape[0]),y] is a vector
+        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
+        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
+        # the mean (across minibatch examples) of the elements in v,
+        # i.e., the mean log-likelihood across the minibatch.
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def errors(self, y):
+        """Return a float representing the number of errors in the minibatch
+        over the total number of examples of the minibatch ; zero one
+        loss over the size of the minibatch
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+        """
+        return T.mean(T.neq(self.y_pred, y))
+
+
+class HiddenLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.nnet.relu):
+        """
+        Typical hidden layer of a MLP: Weight matrix W is of shape (n_in,n_out)
         and the bias vector b is of shape (n_out,).
 
-        NOTE : The nonlinearity used here is tanh
 
-        Hidden unit activation is given by: tanh(dot(input,W) + b)
+        Hidden unit activation is given by: Relu(dot(input,W) + b)
 
         :type rng: numpy.random.RandomState
         :param rng: a random number generator used to initialize weights
@@ -190,32 +161,12 @@ class HiddenLayer(object):
                            layer
         """
         self.input = input
-        # end-snippet-1
 
-        # `W` is initialized with `W_values` which is uniformely sampled
-        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
-        # for tanh activation function
-        # the output of uniform if converted using asarray to dtype
-        # theano.config.floatX so that the code is runable on GPU
-        # Note : optimal initialization of weights is dependent on the
-        #        activation function used (among other things).
-        #        For example, results presented in [Xavier10] suggest that you
-        #        should use 4 times larger initial weights for sigmoid
-        #        compared to tanh
-        #        We have no info for other function, so we use the same as
-        #        tanh.
         if W is None:
-            W_values = numpy.asarray(
-                rng.uniform(
-                    low=-numpy.sqrt(2. / (n_in + n_out)),
-                    high=numpy.sqrt(2. / (n_in + n_out)),
-                    size=(n_in, n_out)
-                ),
-                dtype=theano.config.floatX
-            )
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
-
+            # initializing W with values that are convenient for Relu activation function
+            W_values = numpy.asarray(rng.uniform(low=-numpy.sqrt(2. / (n_in + n_out)),
+                                                 high=numpy.sqrt(2. / (n_in + n_out)),
+                                                 size=(n_in, n_out)), dtype=theano.config.floatX)
             W = theano.shared(value=W_values, name='W', borrow=True)
 
         if b is None:
@@ -225,26 +176,15 @@ class HiddenLayer(object):
         self.W = W
         self.b = b
 
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (
-            lin_output if activation is None
-            else activation(lin_output)
-        )
+        linear_output = T.dot(input, self.W) + self.b
+        self.output = activation(linear_output)
         # parameters of the model
         self.params = [self.W, self.b]
 
 
-# start-snippet-2
-class MLP(object):
-    """Multi-Layer Perceptron Class
 
-    A multilayer perceptron is a feedforward artificial neural network model
-    that has one layer or more of hidden units and nonlinear activations.
-    Intermediate layers usually have as activation function tanh or the
-    sigmoid function (defined here by a ``HiddenLayer`` class)  while the
-    top layer is a softmax layer (defined here by a ``LogisticRegression``
-    class).
-    """
+class MLP(object):
+    """Multi-Layer Perceptron Class """
 
     def __init__(self, rng, input, n_in, n_hidden, n_out):
         """Initialize the parameters for the multilayer perceptron
@@ -269,10 +209,6 @@ class MLP(object):
 
         """
 
-        # Since we are dealing with a one hidden layer MLP, this will translate
-        # into a HiddenLayer with a tanh activation function connected to the
-        # LogisticRegression layer; the activation function can be replaced by
-        # sigmoid or any other nonlinear function
         self.hiddenLayer = HiddenLayer(
             rng=rng,
             input=input,
@@ -288,20 +224,10 @@ class MLP(object):
             n_in=n_hidden,
             n_out=n_out
         )
-        # end-snippet-2 start-snippet-3
         # L1 norm ; one regularization option is to enforce L1 norm to
         # be small
-        self.L1 = (
-            abs(self.hiddenLayer.W).sum()
-            + abs(self.logRegressionLayer.W).sum()
-        )
-
-        # square of L2 norm ; one regularization option is to enforce
-        # square of L2 norm to be small
-        self.L2_sqr = (
-            (self.hiddenLayer.W ** 2).sum()
-            + (self.logRegressionLayer.W ** 2).sum()
-        )
+        # square of L2 norm;
+        self.L2_sqr = ((self.hiddenLayer.W ** 2).sum() + (self.logRegressionLayer.W ** 2).sum())
 
         # negative log likelihood of the MLP is given by the negative
         # log likelihood of the output of the model, computed in the
@@ -321,7 +247,7 @@ class MLP(object):
         self.input = input
 
 
-def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=5,
+def test_mlp(learning_rate=0.01, L2_reg=0.0001, n_epochs=5,
              dataset='mnist.pkl.gz', batch_size=20, n_hidden=500):
     """
     Demonstrate stochastic gradient descent optimization for a multilayer
@@ -389,7 +315,6 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=5,
     # here symbolically
     cost = (
         classifier.negative_log_likelihood(y)
-        + L1_reg * classifier.L1
         + L2_reg * classifier.L2_sqr
     )
     # end-snippet-4
@@ -561,5 +486,5 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=5,
 
 
 if __name__ == '__main__':
-    valid_rec, train_rec, test_rec=test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=10, n_hidden=500)
+    valid_rec, train_rec, test_rec=test_mlp(learning_rate=0.01, L2_reg=0.0001, n_epochs=10, n_hidden=500)
     records_file_maker("mlp1ed_hl200_lr01_reg0001.csv", train_rec, valid_rec, test_rec)
